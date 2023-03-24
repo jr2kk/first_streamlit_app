8bcd3fb01cc998559cbb695ab8521f37f3831311
jr2kk <118006078+jr2kk@users.noreply.github.com>
2023-03-24 11:31:17 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@ import streamlit
 streamlit.title('my first web page app')
 streamlit.header('hello there')
 streamlit.text('hi how are you?')
+
+streamlit.title('Diner')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥣Hard-Boiled Free-Range Egg')
+
